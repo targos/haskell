bc87ba398c0f643355f6736d79a50a422fc8474f
Michaël Zasso <targos@protonmail.com>
2024-12-04 08:14:26 +0100

    simplify primes

--- challenges/primes.hs
+++ challenges/primes.hs
@@ -1,7 +1,7 @@
 primeCandidates = 2:[3,5..]
-isPrime n = all (\x -> n `mod` x /= 0) (takeWhile (\x -> x*x <= n) primeCandidates)
-primes = [n | n <- primeCandidates, isPrime n]
-result = primes !! 99_999
+isPrime n = all (\x -> (n `mod` x) /= 0) (takeWhile (\x -> x*x <= n) primeCandidates)
+primes = filter isPrime primeCandidates
+nthPrime n = primes !! (n-1)
 
 main :: IO ()
-main = print result
+main = print (nthPrime 100_000)
