a9de655533fb648fe9e85d670a4e5dafa192e889
Michaël Zasso <targos@protonmail.com>
2024-12-07 22:55:39 +0100

    hof

--- learn-you/05-hof.hs
+++ learn-you/05-hof.hs
@@ -1,47 +1,75 @@
-multThree :: (Num a) => a -> a -> a -> a  
-multThree x y z = x * y * z 
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
 
 test = multThree 3 5
 test2 = test 9
 
-compareWithHundred :: (Num a, Ord a) => a -> Ordering  
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
 compareWithHundred = compare 100
 
-divideByTen :: (Floating a) => a -> a  
+divideByTen :: (Floating a) => a -> a
 divideByTen = (/10)
 
 isUpperAlphanum :: Char -> Bool
 isUpperAlphanum = (`elem` ['A'..'Z'])
 
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
-zipWith' _ [] _ = []  
-zipWith' _ _ [] = []  
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
-flip' :: (a -> b -> c) -> (b -> a -> c)  
-flip' f = g  
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
     where g x y = f y x
 
-flip'' :: (a -> b -> c) -> b -> a -> c  
-flip'' f y x = f x y 
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f y x = f x y
 
-map' :: (a -> b) -> [a] -> [b]  
-map' _ [] = []  
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
 map' f (x:xs) = f x : map' f xs
 
-filter' :: (a -> Bool) -> [a] -> [a]  
-filter' _ [] = []  
-filter' p (x:xs)  
-    | p x       = x : filter' p xs  
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
     | otherwise = filter' p xs
 
-quicksort :: (Ord a) => [a] -> [a]  
-quicksort [] = []  
-quicksort (x:xs) = 
-    let smallerSorted = quicksort (filter (<=x) xs)  
-        biggerSorted = quicksort (filter (>x) xs)  
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)
+        biggerSorted = quicksort (filter (>x) xs)
     in  smallerSorted ++ [x] ++ biggerSorted
 
-largestDivisible :: (Integral a) => a  
-largestDivisible = head (filter p [100000,99999..])  
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
     where p x = x `mod` 3829 == 0
+
+--  find the sum of all odd squares that are smaller than 10,000
+result = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+-- Collatz sequence starting from a.
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter longerThan15 (map chain [1..100]))
+    where longerThan15 x = length x > 15
+
+numLongChains2 :: Int
+numLongChains2 = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum' :: (Num a) => [a] -> a
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+-- Interesting but bad implementation that doesn't short-circuit.
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+inverse :: [a] -> [a]
+inverse = foldl (\acc x -> x:acc) []
--- learn-you/05-hof.md
+++ learn-you/05-hof.md
@@ -7,3 +7,44 @@ Notes from <https://learnyouahaskell.github.io/higher-order-functions.html>.
 * All functions only have one "real" parameter.
 * When we pass less than the expected number of parameters, we get a partially applied function in return.
 * Infix functions can be partially applied.
+
+## Maps and filters
+
+* `map` Takes a function and a list and produces a list by applying the function.
+* `filter` Takes a predicate and a list and produces a list filtered by the predicate.
+* `takeWhile` Takes a predicate and a list and produces a list that stops when the predicate returns False.
+
+## Lambdas
+
+* Lambdas are just anonymous functions that we can pass to higher-order functions.
+* Definition: `(\param1 param2 -> result)`.
+* Parenthesis is not mandatory but here to avoid full extension to the right.
+* You can use pattern matching in lambdas but the pattern must always match.
+
+## Folds
+
+* A fold takes a binary function, a starting value and a list to fold up.
+* The binary function is called with the accumulator and the first (or last) element and produces a new accumulator
+* This is basically a reducer.
+* `foldl` (left fold) folds a list from the left side.
+* `foldr` (right fold) folds from the right side, so the accumulator value is passed second to the binary function.
+  * Usually `foldr` is better because it works on infinite lists.
+* `foldl1` and `foldr1` are similar but take the first element as the starting value. Need at least one element in the list.
+* `scanl`, `scanl1` and `scanr`, `scanr1` are like the folds but keep all values and generate a new list.
+
+## Function application
+
+* The `$` function is called function application.
+* Function application with a space is left-associative (so `f a b c` is the same as `((f a) b) c)`).
+* function application with $ is right-associative.
+* Example: `sqrt $ 3 + 4 + 9` vs `sqrt (3 + 4 + 9)`.
+* We can map function application over a list of functions: `map ($ 3) [(4+), (10*), (^2), sqrt]`.
+
+## Function composition
+
+* Equivalent to the mathematical `(f°g)(x)=f(g(x))`.
+* Done with the `.` function.
+* `map (negate . abs) [5,-3,-6,7,-3,2,-19,24]`.
+* `map (negate . sum . tail) [[1..5],[3..6],[1..7]]`.
+* To use it with functions that take more than one parameter, we must partially apply them first.
+* `fn = ceiling . negate . tan . cos . max 50` vs `fn x = ceiling (negate (tan (cos (max 50 x))))`.
