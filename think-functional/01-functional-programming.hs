57073f6d15879458fa05d13e4d5b60ef0fcaaff3
Michaël Zasso <targos@protonmail.com>
2025-12-02 22:22:56 +0100

    ch1

---
+++ think-functional/01-functional-programming.hs
@@ -0,0 +1,75 @@
+import Data.Char
+
+type Text = [Char]
+
+type Word' = [Char]
+
+map' :: (a -> b) -> [a] -> [b]
+map' = map
+
+take' :: Int -> [a] -> [a]
+take' a list = []
+
+commonWords :: Int -> Text -> String
+commonWords n =
+  concatMap showRun
+    . take n
+    . sortRuns
+    . countRuns
+    . sortWords
+    . words
+    . map toLower
+
+units, teens, tens :: [String]
+units =
+  [ "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  ]
+teens =
+  [ "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+  ]
+tens =
+  [ "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+  ]
+
+convert1 :: Int -> String
+convert1 num = units !! num
+
+digits2 :: Int -> (Int, Int)
+digits2 n = (n `div` 10, n `mod` 10)
+
+convert2 :: Int -> String
+convert2 n
+  | t == 0 = units !! u
+  | t == 1 = teens !! u
+  | u == 0 = tens !! (t - 2)
+  | otherwise = tens !! (t - 2) ++ "-" ++ units !! u
+  where
+    (t, u) = (n `div` 10, n `mod` 10)
+
+double :: Integer -> Integer
+double x = 2 * x
